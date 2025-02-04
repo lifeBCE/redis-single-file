6ae4090d4ce3f250fdced2a2b73dfb60c33c897f
LifeBCE <eric06@gmail.com>
2025-02-03 21:50:25 -0600

    Linting cleanup

--- Gemfile
+++ Gemfile
@@ -5,13 +5,13 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in redis_single_file.gemspec
 gemspec
 
+gem 'benchmark', '~> 0.4.0'
+gem 'benchmark-ips', '~> 2.14.0'
+gem 'mock_redis', '~> 0.49.0'
 gem 'pry', '~> 0.15.2'
 gem 'rake', '~> 13.0'
 gem 'rspec', '~> 3.0'
 gem 'rubocop', '~> 1.21'
+gem 'rubocop-factory_bot', '~> 2.26.1'
 gem 'rubocop-rake', '~> 0.6.0'
 gem 'rubocop-rspec', '~> 3.4.0 '
-gem 'rubocop-factory_bot', '~> 2.26.1'
-gem 'mock_redis', '~> 0.49.0'
-gem 'benchmark', '~> 0.4.0'
-gem 'benchmark-ips', '~> 2.14.0'
--- benchmark.rb
+++ benchmark.rb
@@ -3,8 +3,8 @@
 require 'benchmark/ips'
 require 'redis_single_file'
 
-scenario_1_semaphore = RedisSingleFile.new(name: :scenario_1)
-scenario_2_semaphore = RedisSingleFile.new(name: :scenario_2)
+scenario_1_semaphore = RedisSingleFile.new(name: :scenario1)
+scenario_2_semaphore = RedisSingleFile.new(name: :scenario2)
 
 Benchmark.ips do |x|
   x.report('synchronize') do
@@ -18,20 +18,18 @@ Benchmark.ips do |x|
   x.report('threaded (10x)') do
     threads = 10.times.map do
       Thread.new do
-        scenario_3_semaphore = RedisSingleFile.new(name: :scenario_3)
+        scenario_3_semaphore = RedisSingleFile.new(name: :scenario3)
         scenario_3_semaphore.synchronize { nil }
       end
     end
 
-    while threads.any?(&:alive?) do
-      threads.each { _1.join(0.05) }
-    end
+    threads.each { _1.join(0.05) } while threads.any?(&:alive?)
   end
 
   x.report('forked (10x)') do
     10.times.each do
       fork do
-        scenario_4_semaphore = RedisSingleFile.new(name: :scenario_4)
+        scenario_4_semaphore = RedisSingleFile.new(name: :scenario4)
         scenario_4_semaphore.synchronize { nil }
       end
     end
